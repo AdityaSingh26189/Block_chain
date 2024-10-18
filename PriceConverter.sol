7476ed101a474f20bed74701d741668dcb2e4c12
Aditya Singh <165035425+AdityaSingh26189@users.noreply.github.com>
2024-10-18 17:20:05 +0530

    Update PriceConverter.sol

--- PriceConverter.sol
+++ PriceConverter.sol
@@ -1,24 +1,22 @@
-// SPDX-License-Identifier: MIT
+
 pragma solidity ^0.8.8;
 
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
 library PriceConverter {
-    // We could make this public, but then we'd have to deploy it
+    
     function getPrice() internal view returns (uint256) {
-        // Sepolia ETH / USD Address
-        // https://docs.chain.link/data-feeds/price-feeds/addresses#Sepolia%20Testnet
+
         AggregatorV3Interface priceFeed = AggregatorV3Interface(
             0x88A85688089C15ce061Cba7abd17913dc9A6F8cf
         );
         (, int256 answer, , , ) = priceFeed.latestRoundData();
-        // ETH/USD rate in 18 digit
+       
         return uint256(answer * 10000000000);
-        // or (Both will do the same thing)
-        // return uint256(answer * 1e10); // 1* 10 ** 10 == 10000000000
+        
     }
 
-    // 1000000000
+
     function getConversionRate(uint256 ethAmount)
         internal
         view
@@ -26,9 +24,6 @@ library PriceConverter {
     {
         uint256 ethPrice = getPrice();
         uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1000000000000000000;
-        // or (Both will do the same thing)
-        // uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1e18; // 1 * 10 ** 18 == 1000000000000000000
-        // the actual ETH/USD conversion rate, after adjusting the extra 0s.
         return ethAmountInUsd;
     }
 }
